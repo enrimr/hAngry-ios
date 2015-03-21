5bdebcbe49c4058664ab810786f2154545d9f2a4
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2015-03-21 10:17:21 +0100

    Initial Commit

---
+++ hAngry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hAngry
+//
+//  Created by Enrique Ismael Mendoza Robaina on 21/3/15.
+//  Copyright (c) 2015 Atlanpix SLU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
